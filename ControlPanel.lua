142fc315e927a547e0891bbb721458f992660580
ChillBoi <49378907+ChillBoi@users.noreply.github.com>
2021-08-10 20:19:30 -0700

    Update and rename remoteredstone.lua to ControlPanel.lua

---
+++ ControlPanel.lua
@@ -0,0 +1,7 @@
+print("Welcome to the Control Panel! You can configure your network or manage your devices.")
+print("1: Configure Network \n2: Manage Devices")
+if io.read() == "1"
+  print("Welcome to the Network Configurator! Would you like to add or remove a device?")
+elif io.read() == "2"
+  print("Welcome to the Device Manager! What would you like to activate or deactivate?")
+end
